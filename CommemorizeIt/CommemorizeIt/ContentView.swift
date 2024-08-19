8afb8af1a943a5efa722a509c7da463cb48ba32b
Gimli Kim <infinite1st@gmail.com>
2024-08-19 22:25:30 +0900

    [test]
    test다.

--- CommemorizeIt/CommemorizeIt/ContentView.swift
+++ CommemorizeIt/CommemorizeIt/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "star")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("나는 할 수 있어! 뿡")
         }
         .padding()
     }
