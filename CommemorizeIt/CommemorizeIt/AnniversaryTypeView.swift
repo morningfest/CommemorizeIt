f83d227b6e45dbb68bf3a83873f8d345eb9e024f
Gimli Kim <infinite1st@gmail.com>
2024-09-20 00:13:14 +0900

    [Design] 기념일 종류 선택 뷰를 만들었어요
    엑스코드가 배경 색상 자꾸 홈이랑 조건 똑같은데 위 아래 계속 비워놔서 이짓저짓 다하다가 잘 해결했어요. 근데 아직도 열받아요.

---
+++ CommemorizeIt/CommemorizeIt/AnniversaryTypeView.swift
@@ -0,0 +1,38 @@
+//
+//  AnniversaryTypeView.swift
+//  CommemorizeIt
+//
+//  Created by kyunglimkim on 9/19/24.
+// 엑스코드 쓰레기 진짜 이시끼 에러가 왤케 많음? 진짜 개큰 짜증난다.
+
+import SwiftUI
+
+struct AnniversaryTypeView: View {
+    var body: some View {
+            VStack(alignment: .leading, spacing: 0) {
+                Image("prgb1")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.bottom, 50)
+                
+                Text("김리대통령님\n오늘 어떤 기념일로 지정할까요?")
+                    .normalTitleFont()
+                
+                AnniversaryTypeListView()
+                    .padding(.top, 32)
+                
+                Spacer()
+                    .frame(height: 70)
+                
+                MainButton(text: "다음")
+                
+                Spacer()
+            }
+        .padding(.horizontal, 26)
+        .background(.backround)
+    }
+}
+
+#Preview {
+    AnniversaryTypeView()
+}
